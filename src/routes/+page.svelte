8691671afbab369d535ff5d26b2f774b00eb698d
giulio gherardi <giuliogherardi@Mac.fritz.box>
2026-07-11 01:16:10 +0200

    initial setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+</script>
+
+<div class="container mx-auto p-8">
+  <h1 class="text-4xl font-bold">Welcome to My Stories</h1>
+  <p class="mt-4 text-lg">Your SvelteKit application is ready!</p>
+</div>
